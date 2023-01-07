785898c6aba11b7ab3375ce59101a4bf09a973be
Yena Lee <lmonari71@gmail.com>
2023-01-08 01:41:32 +0900

    Peach-로그인,회원가입 viewcontroller 연결

---
+++ WHAT_IS_MONEY/FindIdPwViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FindIdPwViewController.swift
+//  WHAT_IS_MONEY
+//
+//  Created by 이예나 on 1/8/23.
+//
+
+import UIKit
+
+class FindIdPwViewController: UIViewController {
+
+    @IBOutlet weak var EmailInput: UITextField!
+    @IBOutlet weak var IdInput: UITextField!
+    @IBOutlet weak var EmailInputForPw: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func findIDclicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func findPWclicked(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
