51516ecb17014f2d168c5c8077591759dfd2f7dd
jinyongyun <dbswlsdyd730@naver.com>
2023-01-12 23:48:02 +0900

    dobby2

---
+++ WHAT_IS_MONEY/Record.swift
@@ -0,0 +1,8 @@
+//
+//  Record.swift
+//  WHAT_IS_MONEY
+//
+//  Created by jinyong yun on 2023/01/12.
+//
+
+import Foundation
---
+++ WHAT_IS_MONEY/WriteRecordViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WriteRecordViewController.swift
+//  WHAT_IS_MONEY
+//
+//  Created by jinyong yun on 2023/01/12.
+//
+
+import Foundation
