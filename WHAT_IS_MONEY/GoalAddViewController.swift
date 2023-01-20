0277a25122c5fd7898f3b51cb7357354b57a746e
jinyongyun <dbswlsdyd730@naver.com>
2023-01-20 17:44:54 +0900

    피치파일수정

---
+++ WHAT_IS_MONEY/GoalAddViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GoalAddViewController.swift
+//  WHAT_IS_MONEY
+//
+//  Created by jinyong yun on 2023/01/20.
+//
+
+import UIKit
+
+class GoalAddViewController: UIViewController {
+
+    @IBOutlet weak var ImgUI: UIButton!
+    @IBOutlet weak var GoalTextField: UITextField!
+    @IBOutlet weak var GoalPriceTextField: UITextField!
+    @IBOutlet weak var InitPriceTextField: UITextField!
+    @IBOutlet weak var CompleteButtton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
