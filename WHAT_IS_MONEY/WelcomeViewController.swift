7bf50021ddd6a8f7d00d2f0d201e03531105e3f6
Yena Lee <53147797+YENA0512@users.noreply.github.com>
2023-02-05 21:10:59 +0900

    프로필사진 api 연결 (#30)
    * profile image api 수정

--- WHAT_IS_MONEY/WelcomeViewController.swift
+++ WHAT_IS_MONEY/WelcomeViewController.swift
@@ -22,82 +22,9 @@ class WelcomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true // 뷰 컨트롤러가 나타날 때 숨기기
         TokenClass.handlingToken()
-        getUserProfile()
         getUser()
     }
     
-    func getUserProfile() {
-        let useridx = UserDefaults.standard.integer(forKey: "userIdx")
-        let accessToken = UserDefaults.standard.string(forKey: "accessToken")
-        print(useridx)
-        guard let url = URL(string: "https://www.pigmoney.xyz/users/profileImage/\(useridx)") else {
-            print("Error: cannot create URL")
-            return
-        }
-        // Create the url request
-        var request = URLRequest(url: url)
-        
-        request.httpMethod = "GET"
-        request.addValue(accessToken!, forHTTPHeaderField: "X-ACCESS-TOKEN")
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                print("Error: error calling GET")
-                print(error!)
-                return
-            }
-            guard let data = data else {
-                print("Error: Did not receive data")
-                return
-            }
-            
-            print(String(data: data, encoding: .utf8)!)
-            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
-                print("Error: HTTP request failed")
-                return
-            }
-            DispatchQueue.main.async {
-                do {
-                    guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                        print("Error: Cannot convert data to JSON object")
-                        return
-                    }
-                    guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
-                        print("Error: Cannot convert JSON object to Pretty JSON data")
-                        return
-                    }
-                    guard String(data: prettyJsonData, encoding: .utf8) != nil else {
-                        print("Error: Couldn't print JSON in String")
-                        return
-                    }
-                    
-                    guard let result = jsonObject ["result"] as? String
-                    else { return }
-                    print("imgresult",result)
-                    print(result.count)
-                    if (result.count == 0) {
-                        self.ProfileImg.image = UIImage(named: "jinperson")
-                        
-                    } else {
-                        if let data = Data(base64Encoded: result, options: .ignoreUnknownCharacters) {
-                            let decodedImg = UIImage(data: data)
-                            self.ProfileImg.layer.cornerRadius = self.ProfileImg.frame.height/2
-                            self.ProfileImg.layer.borderWidth = 1
-                            self.ProfileImg.layer.borderColor = UIColor.clear.cgColor
-                            // 뷰의 경계에 맞춰준다
-                            self.ProfileImg.clipsToBounds = true
-                            self.ProfileImg.image = decodedImg
-                            self.ProfileImg.image = decodedImg
-                            
-                        }
-                    }
-                } catch {
-                    print("Error: Trying to convert JSON data to string")
-                    return
-                }
-            }
-            
-        }.resume()
-    }
     
     func getUser() {
         let useridx = UserDefaults.standard.integer(forKey: "userIdx")
@@ -148,8 +75,27 @@ class WelcomeViewController: UIViewController {
                     guard let result = jsonObject ["result"] as? [String: Any]
                     else { return }
                     let name = result ["name"] as? String
+                    let image = result ["image"] as? String
                     
-                    print(result)
+     
+                    
+                    if (image?.count == 0 || image == nil) {
+                        print(image as Any)
+                        self.ProfileImg.image = UIImage(named: "jinperson")
+                        
+                    } else {
+                        if let data = Data(base64Encoded: image!, options: .ignoreUnknownCharacters) {
+                            let decodedImg = UIImage(data: data)
+                            self.ProfileImg.layer.cornerRadius = self.ProfileImg.frame.height/2
+                            self.ProfileImg.layer.borderWidth = 1
+                            self.ProfileImg.layer.borderColor = UIColor.clear.cgColor
+                            // 뷰의 경계에 맞춰준다
+                            self.ProfileImg.clipsToBounds = true
+                            self.ProfileImg.image = decodedImg
+                            self.ProfileImg.image = decodedImg
+                            
+                        }
+                    }
                     
                     self.IntroLabel.text = "\(name ?? "머니뭐니")님, 어서오세요"
                     
--- WHAT_IS_MONEY/WholeSettingViewController.swift
+++ WHAT_IS_MONEY/WholeSettingViewController.swift
@@ -45,81 +45,9 @@ class WholeSettingViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.isHidden = false
         TokenClass.handlingToken()
-        getUserProfile()
         getUserInfo()
     }
-    func getUserProfile() {
-        let useridx = UserDefaults.standard.integer(forKey: "userIdx")
-        let accessToken = UserDefaults.standard.string(forKey: "accessToken")
-        print(useridx)
-        guard let url = URL(string: "https://www.pigmoney.xyz/users/profileImage/\(useridx)") else {
-            print("Error: cannot create URL")
-            return
-        }
-        // Create the url request
-        var request = URLRequest(url: url)
-        
-        request.httpMethod = "GET"
-        request.addValue(accessToken!, forHTTPHeaderField: "X-ACCESS-TOKEN")
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                print("Error: error calling GET")
-                print(error!)
-                return
-            }
-            guard let data = data else {
-                print("Error: Did not receive data")
-                return
-            }
-            
-            print(String(data: data, encoding: .utf8)!)
-            guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
-                print("Error: HTTP request failed")
-                return
-            }
-            DispatchQueue.main.async {
-                do {
-                    guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                        print("Error: Cannot convert data to JSON object")
-                        return
-                    }
-                    guard let prettyJsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted) else {
-                        print("Error: Cannot convert JSON object to Pretty JSON data")
-                        return
-                    }
-                    guard String(data: prettyJsonData, encoding: .utf8) != nil else {
-                        print("Error: Couldn't print JSON in String")
-                        return
-                    }
-                    
-                    guard let result = jsonObject ["result"] as? String
-                    else { return }
-                    print("imgresult",result)
-                    print(result.count)
-                    if (result.count == 0) {
-                        self.ProfileImg.image = UIImage(named: "jinperson")
-                        
-                    } else {
-                        if let data = Data(base64Encoded: result, options: .ignoreUnknownCharacters) {
-                            let decodedImg = UIImage(data: data)
-                            self.ProfileImg.layer.cornerRadius = self.ProfileImg.frame.height/2
-                            self.ProfileImg.layer.borderWidth = 1
-                            self.ProfileImg.layer.borderColor = UIColor.clear.cgColor
-                            // 뷰의 경계에 맞춰준다
-                            self.ProfileImg.clipsToBounds = true
-                            self.ProfileImg.image = decodedImg
-                            print("img attached")
-                            
-                        }
-                    }
-                } catch {
-                    print("Error: Trying to convert JSON data to string")
-                    return
-                }
-            }
-            
-        }.resume()
-    }
+
     func getUserInfo() {
         let useridx = UserDefaults.standard.integer(forKey: "userIdx")
         let accessToken = UserDefaults.standard.string(forKey: "accessToken")
@@ -168,9 +96,26 @@ class WholeSettingViewController: UIViewController {
                         else { return }
                         let name = result ["name"] as? String
                         let id = result ["userId"] as? String
+                        let image = result ["image"] as? String
                       
                         print(result)
                     
+                        if (image?.count == 0 || image == nil) {
+                            self.ProfileImg.image = UIImage(named: "jinperson")
+                            
+                        } else {
+                            if let data = Data(base64Encoded: image!, options: .ignoreUnknownCharacters) {
+                                let decodedImg = UIImage(data: data)
+                                self.ProfileImg.layer.cornerRadius = self.ProfileImg.frame.height/2
+                                self.ProfileImg.layer.borderWidth = 1
+                                self.ProfileImg.layer.borderColor = UIColor.clear.cgColor
+                                // 뷰의 경계에 맞춰준다
+                                self.ProfileImg.clipsToBounds = true
+                                self.ProfileImg.image = decodedImg
+                                print("img attached")
+                                
+                            }
+                        }
                         self.UserNameLabel.text = name
                         self.UserIDLabel.text = id
                      
